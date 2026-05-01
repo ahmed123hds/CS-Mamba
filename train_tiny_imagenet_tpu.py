7bc7cafc7a69b055a0fc05914737ca47cc0fce12
ahmed123hds <kodek334@gmail.com>
2026-05-01 16:46:06 +0900

    Ablation: hardcode use_mixup to False to disable all mixing

--- train_tiny_imagenet_tpu.py
+++ train_tiny_imagenet_tpu.py
@@ -345,7 +345,9 @@ def _mp_fn(index, flags):
         shuffle=False
     )
 
-    use_mixup = not getattr(flags, 'no_mixup', False)
+    # [ABLATION] Commented out Mixup/CutMix entirely
+    # use_mixup = not getattr(flags, 'no_mixup', False)
+    use_mixup = False
     mixup_collate = MixupCutMixCollate(n_classes=flags.n_classes) if use_mixup else None
 
     train_loader_raw = DataLoader(
